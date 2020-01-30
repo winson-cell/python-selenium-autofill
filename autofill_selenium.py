118f56a6262de0789e919ceaac831df31361e413
winson-cell <59526011+winson-cell@users.noreply.github.com>
2020-01-30 10:49:38 +0800

    Update autofill_selenium.py

--- autofill_selenium.py
+++ autofill_selenium.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 def GOURL():
     URL = ip.get()
-    driver = webdriver.Chrome("direntory of chromedriver\\chromedriver.exe")
+    driver = webdriver.Chrome("directory of chromedriver\\chromedriver.exe")
     driver.get(URL)
     driver.maximize_window()
     fo = open("name_key.txt","r+")
@@ -18,14 +18,12 @@ def GOURL():
             driver.find_element_by_name(name).send_keys(key)
         except:
             pass
+        
 # creating a window
 mGui = Tk()
 ip = StringVar()
 mGui.geometry('450x450')
 mGui.title('URL')
-
 mlabel = Label(mGui,text = "Please insert URL").pack()
-
 mEntry = Entry(mGui,width = 70,textvariable = ip).pack()
-
 mbutton = Button(mGui , text = "OK",command = GOURL,fg = 'black',bg = 'grey').pack()
