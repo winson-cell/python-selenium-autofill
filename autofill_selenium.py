6f20a84c0e4826c2844b2289185936bce3c64878
winson-cell <59526011+winson-cell@users.noreply.github.com>
2020-01-30 10:23:30 +0800

    Add files via upload

---
+++ autofill_selenium.py
@@ -0,0 +1,31 @@
+import sys
+from tkinter import *
+from selenium import webdriver
+
+def GOURL():
+    URL = ip.get()
+    driver = webdriver.Chrome("direntory of chromedriver\\chromedriver.exe")
+    driver.get(URL)
+    driver.maximize_window()
+    fo = open("name_key.txt","r+")
+    context = fo.read()
+    context2 = context.split("\n")
+    counter = len(context2)
+    for i in range(0,counter,2):
+        name = context2[i]
+        key = context2[i+1]
+        try:
+            driver.find_element_by_name(name).send_keys(key)
+        except:
+            pass
+# creating a window
+mGui = Tk()
+ip = StringVar()
+mGui.geometry('450x450')
+mGui.title('URL')
+
+mlabel = Label(mGui,text = "Please insert URL").pack()
+
+mEntry = Entry(mGui,width = 70,textvariable = ip).pack()
+
+mbutton = Button(mGui , text = "OK",command = GOURL,fg = 'black',bg = 'grey').pack()
